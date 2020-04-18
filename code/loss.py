f0d2893204fb3241cc7f2d65e2421ae86751e6ae
Tianduo Wang <wtianduo@gmail.com>
2020-04-18 11:42:32 +0800

    change identation

--- code/loss.py
+++ code/loss.py
@@ -3,18 +3,18 @@ import torch.nn.functional as F
 
 
 class DenseCrossEntropy(nn.Module):
-	def __init__(self):
-		super(DenseCrossEntropy, self).__init__()
+    def __init__(self):
+        super(DenseCrossEntropy, self).__init__()
 
-	def forward(self, logits, labels):
-		logits = logits.float()
-		labels = labels.float()
+    def forward(self, logits, labels):
+        logits = logits.float()
+        labels = labels.float()
 
-		log_probs = F.log_softmax(logit, dim=-1)
-		loss = -label * log_probs
-		loss = loss.sum(-1)
-		return loss.mean()
+        log_probs = F.log_softmax(logit, dim=-1)
+        loss = -label * log_probs
+        loss = loss.sum(-1)
+        return loss.mean()
 
 
 def get_loss_fn():
-	return DenseCrossEntropy()
+    return DenseCrossEntropy()
